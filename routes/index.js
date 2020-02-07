c2d0cce88c2081905ba71ec075a763070fd296f5
creyes <creyes@technisys.com>
2020-02-07 16:09:19 -0300

    fix: express-validator/check deprecated

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //importar express validator
-const { body } = require('express-validator/check');
+const { body } = require('express-validator');
 
 //importar el controlador
 const proyectosController = require('../controllers/proyectosController');
